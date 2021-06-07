2f84cda4571ed2340731fd9e27d24f21971a6b99
Joao Cunha <joaocunhask@gmail.com>
2021-06-07 06:06:13 -0300

    Foreign key not null (referential integrity)

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -14,6 +14,7 @@ namespace SalesWebMvc.Models
         public double BaseSalary { get; set; }
         public Departament Departament { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+        public int DepartamentID { get; set; }
 
         public Seller()
         {
